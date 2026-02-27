7583ec68f0f4ce5b541a448598088a32cb90fc18
Nicholas Lachapelle <Nicholas.lachapelle@outlook.com>
2026-02-27 10:26:32 -0500

    Initial Commit

---
+++ EverEra/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  EverEra
+//
+//  Created by Nicholas Lachapelle on 2026-02-27.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
